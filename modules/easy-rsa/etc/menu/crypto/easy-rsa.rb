63befd674c6667fc9642253bc00579e5bab7bd69
Guido De Rosa <guidoderosa@gmail.com>
2019-11-09 22:46:38 +0000

    a dedicated pki for external/imported cers/keys

--- modules/easy-rsa/etc/menu/crypto/easy-rsa.rb
+++ modules/easy-rsa/etc/menu/crypto/easy-rsa.rb
@@ -6,10 +6,19 @@ class OnBoard
 end
 
 class OnBoard
+  MENU_ROOT.add_path('/crypto/external', {
+    :href => '/crypto/easy-rsa/external',
+    :name => 'External/Imported Certificates and Keys',
+    #:desc => 'Public Key Infrastructures and own Certificate Authorities',
+    :n    => 0
+  })
+end
+
+class OnBoard
   MENU_ROOT.add_path('/crypto/easy-rsa', {
     :href => '/crypto/easy-rsa',
-    :name => 'Key management',
-    :desc => 'Cryptographic certificates for OpenVPN and other applications',
-    :n    => 1
+    :name => 'Own Certificate Authorities / PKIs',
+    #:desc => 'Public Key Infrastructures and own Certificate Authorities',
+    :n    => 2
   })
 end
