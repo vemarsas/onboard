439ab2f1666557758cbfca1d5b6914ae032e9848
Guido De Rosa <guidoderosa@gmail.com>
2019-11-10 02:34:01 +0000

    multipki: external is external

--- modules/easy-rsa/etc/menu/crypto/easy-rsa.rb
+++ modules/easy-rsa/etc/menu/crypto/easy-rsa.rb
@@ -17,7 +17,7 @@ end
 class OnBoard
   MENU_ROOT.add_path('/crypto/easy-rsa', {
     :href => '/crypto/easy-rsa',
-    :name => 'Own Certificate Authorities / PKIs',
+    :name => 'Manage PKIs',
     #:desc => 'Public Key Infrastructures and own Certificate Authorities',
     :n    => 2
   })
